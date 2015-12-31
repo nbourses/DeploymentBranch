94fbf51e51e7c330cd09dbc9b69a4a437c3de4b8
panjwani94 <pratik.panjwani94@gmail.com>
2015-12-31 11:19:43 +0530

    inner oyeok removed

--- Oyeok/app/src/main/java/com/nbourses/oyeok/RPOT/PriceDiscovery/UI/QrCode/CaptureActivityAnyOrientation.java
+++
@@ -1,9 +0,0 @@
-package com.nbourses.oyeok.RPOT.PriceDiscovery.UI.QrCode;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureActivityAnyOrientation extends CaptureActivity {
-
-
-
-}
